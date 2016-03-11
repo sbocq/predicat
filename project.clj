5deedeb73fb220ad8be38152116e74f81f943852
Sébastien Bocq <sebastien.bocq@gmail.com>
2016-03-11 16:42:01 +0100

    Be clojure 1.6.0 friendly

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject predicat "0.1.0"
-  :description "A library to create and compose predicates with traceable failures"
+(defproject predicat "0.1.1-SNAPSHOT"
+  :description "A Clojure library to compose predicate/validation functions"
   :url "https://github.com/sbocq/predicat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
