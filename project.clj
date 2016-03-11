709f06c1e6594c58fc0e965a5455a0b446d90215
Sébastien Bocq <sebastien.bocq@gmail.com>
2016-03-11 18:51:13 +0100

    Release 0.1.2 (second try)

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject predicat "0.1.1"
-  :description "A Clojure library to compose predicate/validation functions"
-  :url "https://github.com/sbocq/predicat"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
