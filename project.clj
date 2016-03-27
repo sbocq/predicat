a16c42a8f560f148a2a491970a7fa17a65c34ea0
Sébastien Bocq <sebastien.bocq@gmail.com>
2016-03-28 00:06:36 +0200

    Release 0.2.2

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject predicat "0.2.1"
+(defproject predicat "0.2.2"
   :description "A Clojure library to compose predicate/validation functions"
   :url "https://github.com/sbocq/predicat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[lein-codox "0.9.4" :exclusions [org.clojure/clojure]]]
-  :codox
-  {:output-path "codox"
-   :source-uri "http://github.com/sbocq/predicat/blob/v{version}/{filepath}#L{line}"}
+  :scm {:name "git"
+        :url  "https://github.com/sbocq/predicat"}
+  :codox {:output-path "codox"
+          :source-uri "http://github.com/sbocq/predicat/blob/v{version}/{filepath}#L{line}"}
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :cloverage {:plugins [[lein-cloverage "1.0.6"]
                                    [lein-shell "0.5.0"]]
